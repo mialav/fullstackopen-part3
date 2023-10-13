bf0bcfc6476fc392bc272004b64e7c80edfe7570
mialav <mia.lavanti@wolt.com>
2023-10-13 16:38:31 +0300

    exercise 3.20

--- models/person.js
+++ models/person.js
@@ -21,7 +21,17 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /\d{2,3}-\d/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+  },
 });
 
 personSchema.set("toJSON", {
